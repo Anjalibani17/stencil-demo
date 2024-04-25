20c0e2d43e227efe080be0e410f0fde892e27ab9
anjalibani <anjali.bani@idx.inc>
2024-04-25 08:56:16 +0100

    m

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -7,27 +7,54 @@ import { Component,Prop,h } from "@stencil/core";
     //tag name contain unique name + -(dash )
     tag:'side-drawer',
     styleUrl:'./side-drawer.css',
-    // 
-    scoped:true
+shadow:true
+   // scoped:true
    
 })
 //class class SideDrawer extends HTMLElement
 //or
  export class SideDrawer {
 @Prop({reflect:true}) title:string;
-@Prop() open:boolean;
+@Prop({reflect:true,mutable:true}) open:boolean;
+onCloseDrawer()
+{
+    console.log("close");
+    console.log(this.open)
+    this.open=false;
+    // it shows warning that props is by default immutable within component but u can change it from outside
+    //bcz stencil has unidirectional data flow so we can resolve it by giving mutable true in props
+    
+}
 
 render(){
-    let content=null;
-    if(this.open)
-        { //just pass open as props in this component
-            content=(<aside>
-            <header> <h1>{this.title}</h1></header>
+
+    // let content=null;
+    // if(this.open)
+    //     { //just pass open as props in this component
+    //         content=(<aside>
+    //         <header> <h1>{this.title}</h1></header>
+           
+    //         {/* . It acts as a placeholder inside your component's template where you can insert content from the outside. */}
+    //         <slot/>
+    //         </aside>)
+    //     }
+    return(
+        <aside>
+      
+            <header> 
+                <h1>{this.title}</h1> 
+             <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+             </header>
+             <section id="tabs">
+                <button class="active">Navigation</button>
+                <button>contact</button>
+
+             </section>
+          
            
             {/* . It acts as a placeholder inside your component's template where you can insert content from the outside. */}
             <slot/>
-            </aside>)
-        }
-    return(content)
+            </aside>
+    )
 }
 }
